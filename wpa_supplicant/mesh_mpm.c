673c9f28fc3f7fa8a5bd657f6fc032058ce94fec
Thomas Pedersen <thomas@noack.us>
2014-09-01 00:23:29 -0400

    mesh: Add mesh robust security network
    This implementation provides:
    
    - Mesh SAE authentication mechanism
    - Key management (set/get PSK)
    - Cryptographic key establishment
    - Enhanced protection mechanisms for robust management frames
    
    Signed-off-by: Javier Lopez <jlopex@gmail.com>
    Signed-off-by: Javier Cardona <javier@cozybit.com>
    Signed-off-by: Jason Mobarak <x@jason.mobarak.name>
    Signed-off-by: Thomas Pedersen <thomas@noack.us>

--- wpa_supplicant/mesh_mpm.c
+++ wpa_supplicant/mesh_mpm.c
@@ -17,6 +17,7 @@
 #include "wpa_supplicant_i.h"
 #include "driver_i.h"
 #include "mesh_mpm.h"
+#include "mesh_rsn.h"
 
 /* TODO make configurable */
 #define dot11MeshMaxRetries 10
@@ -205,7 +206,7 @@ static void mesh_mpm_send_plink_action(struct wpa_supplicant *wpa_s,
 	struct hostapd_data *bss = ifmsh->bss[0];
 	struct mesh_conf *conf = ifmsh->mconf;
 	u8 supp_rates[2 + 2 + 32];
-	u8 *pos;
+	u8 *pos, *cat;
 	u8 ie_len, add_plid = 0;
 	int ret;
 	int ampe = conf->security & MESH_CONF_SEC_AMPE;
@@ -227,6 +228,7 @@ static void mesh_mpm_send_plink_action(struct wpa_supplicant *wpa_s,
 	if (!buf)
 		return;
 
+	cat = wpabuf_mhead_u8(buf);
 	wpabuf_put_u8(buf, WLAN_ACTION_SELF_PROTECTED);
 	wpabuf_put_u8(buf, type);
 
@@ -296,8 +298,17 @@ static void mesh_mpm_send_plink_action(struct wpa_supplicant *wpa_s,
 		wpabuf_put_le16(buf, sta->peer_lid);
 	if (type == PLINK_CLOSE)
 		wpabuf_put_le16(buf, close_reason);
+	if (ampe)
+		mesh_rsn_get_pmkid(wpa_s->mesh_rsn, sta,
+				   wpabuf_put(buf, PMKID_LEN));
 
 	/* TODO HT IEs */
+
+	if (ampe && mesh_rsn_protect_frame(wpa_s->mesh_rsn, sta, cat, buf)) {
+		wpa_msg(wpa_s, MSG_INFO,
+			"Mesh MPM: failed to add AMPE and MIC IE");
+		goto fail;
+	}
 
 	ret = wpa_drv_send_action(wpa_s, wpa_s->assoc_freq, 0,
 				  sta->addr, wpa_s->own_addr, wpa_s->own_addr,
@@ -306,6 +317,7 @@ static void mesh_mpm_send_plink_action(struct wpa_supplicant *wpa_s,
 		wpa_msg(wpa_s, MSG_INFO,
 			"Mesh MPM: failed to send peering frame");
 
+fail:
 	wpabuf_free(buf);
 }
 
@@ -456,6 +468,8 @@ mesh_mpm_auth_peer(struct wpa_supplicant *wpa_s, const u8 *addr)
 	 * the AP code already sets this flag. */
 	sta->flags |= WLAN_STA_AUTH;
 
+	mesh_rsn_init_ampe_sta(wpa_s, sta);
+
 	os_memset(&params, 0, sizeof(params));
 	params.addr = sta->addr;
 	params.flags = WPA_STA_AUTHENTICATED | WPA_STA_AUTHORIZED;
@@ -533,7 +547,10 @@ void wpa_mesh_new_mesh_peer(struct wpa_supplicant *wpa_s, const u8 *addr,
 		return;
 	}
 
-	mesh_mpm_plink_open(wpa_s, sta, PLINK_OPEN_SENT);
+	if (conf->security == MESH_CONF_SEC_NONE)
+		mesh_mpm_plink_open(wpa_s, sta, PLINK_OPEN_SENT);
+	else
+		mesh_rsn_auth_sae_sta(wpa_s, sta);
 }
 
 
@@ -591,6 +608,7 @@ static void mesh_mpm_fsm(struct wpa_supplicant *wpa_s, struct sta_info *sta,
 			 enum plink_event event)
 {
 	struct hostapd_data *hapd = wpa_s->ifmsh->bss[0];
+	struct mesh_conf *conf = wpa_s->ifmsh->mconf;
 	u16 reason = 0;
 
 	wpa_msg(wpa_s, MSG_DEBUG, "MPM " MACSTR " state %s event %s",
@@ -663,6 +681,8 @@ static void mesh_mpm_fsm(struct wpa_supplicant *wpa_s, struct sta_info *sta,
 						   PLINK_CONFIRM, 0);
 			break;
 		case CNF_ACPT:
+			if (conf->security & MESH_CONF_SEC_AMPE)
+				mesh_rsn_derive_mtk(wpa_s, sta);
 			mesh_mpm_plink_estab(wpa_s, sta);
 			break;
 		default:
@@ -756,6 +776,7 @@ void mesh_mpm_action_rx(struct wpa_supplicant *wpa_s,
 {
 	u8 action_field;
 	struct hostapd_data *hapd = wpa_s->ifmsh->bss[0];
+	struct mesh_conf *mconf = wpa_s->ifmsh->mconf;
 	struct sta_info *sta;
 	u16 plid = 0, llid = 0;
 	enum plink_event event;
@@ -823,6 +844,12 @@ void mesh_mpm_action_rx(struct wpa_supplicant *wpa_s,
 
 	if (!sta->my_lid)
 		mesh_mpm_init_link(wpa_s, sta);
+
+	if (mconf->security & MESH_CONF_SEC_AMPE)
+		if (mesh_rsn_process_ampe(wpa_s, sta, &elems,
+					  &mgmt->u.action.category,
+					  ies, ie_len))
+			return;
 
 	if (sta->plink_state == PLINK_BLOCKED)
 		return;
